66817878d1b7a19ad9ae8d49f5aa8b793d8a066a
Bert McCutchen <mail@bertm.dev>
2025-12-27 20:59:37 -0500

    Doc images update

--- script/doc_images.rb
+++ script/doc_images.rb
@@ -1,36 +1,51 @@
 require "puppeteer-ruby"
 
-screenshots_dir = Rails.root.join("docs/assets")
-themes_dir = Rails.root.join("docs/assets/themes")
-FileUtils.mkdir_p(themes_dir)
+PREFIX = ARGV[0]
+WIDTH = ARGV[1].to_i
+HEIGHT = ARGV[2].to_i
 
-def screenshot(browser, url:, path:, wait_for_turbo: false)
-  page = browser.new_page
-  page.viewport = Puppeteer::Viewport.new(width: 1920, height: 1080)
+def reset_cache!
+  Rails.cache.delete("setting/theme")
+  Rails.cache.write("quote/daily_pick", Quote.first)
+end
+
+def change_theme!(token)
+  Setting.find_by(slug: "theme").update!(value: token)
+  reset_cache!
+end
+
+def file_path(dir, name)
+  dir_path = Rails.root.join("docs/#{dir}")
+  FileUtils.mkdir_p(dir_path)
+
+  "#{dir_path}/#{PREFIX}_#{name}.png"
+end
+
+def screenshot(page, url:, path:)
   page.goto(url, wait_until: "networkidle0")
-  page.wait_for_selector('turbo-frame[complete]') if wait_for_turbo
   page.screenshot(path:)
 end
 
-Puppeteer.launch(headless: true, args: [ "--window-size=1920,1080" ]) do |browser|
+Puppeteer.launch(headless: true, args: [ "--window-size=#{WIDTH},#{HEIGHT}", "--disable-cache", "--disk-cache-size=0" ]) do |browser|
+  page = browser.new_page
+  page.viewport = Puppeteer::Viewport.new(width: WIDTH, height: HEIGHT)
+
   # Screenshot the theme editing.
-  Setting.find_by(slug: "theme").update!(value: Theme.find_by(name: "Gruvbox").token)
-  screenshot(browser, url: "http://127.0.0.1:3000/themes/new", path: "#{screenshots_dir}/new_theme.png", wait_for_turbo: true)
+  change_theme!(Theme.find_by(name: "Gruvbox").token)
+  screenshot(page, url: "http://127.0.0.1:3000/themes/new", path: file_path("assets", "new_theme"))
 
   # Screenshot various pages as the default theme.
-  Setting.find_by(slug: "theme").update!(value: Theme.default.token)
-
-  screenshot(browser, url: "http://127.0.0.1:3000/settings", path: "#{screenshots_dir}/settings.png")
-  screenshot(browser, url: "http://127.0.0.1:3000/applications", path: "#{screenshots_dir}/applications.png")
-  screenshot(browser, url: "http://127.0.0.1:3000/bookmarks", path: "#{screenshots_dir}/bookmarks.png")
-  screenshot(browser, url: "http://127.0.0.1:3000/categories", path: "#{screenshots_dir}/categories.png")
-  screenshot(browser, url: "http://127.0.0.1:3000/quotes", path: "#{screenshots_dir}/quotes.png")
-  screenshot(browser, url: "http://127.0.0.1:3000/themes", path: "#{screenshots_dir}/themes.png")
+  change_theme!(Theme.default.token)
+  screenshot(page, url: "http://127.0.0.1:3000/settings", path: file_path("assets", "settings"))
+  screenshot(page, url: "http://127.0.0.1:3000/applications", path: file_path("assets", "applications"))
+  screenshot(page, url: "http://127.0.0.1:3000/bookmarks", path: file_path("assets", "bookmarks"))
+  screenshot(page, url: "http://127.0.0.1:3000/categories", path: file_path("assets", "categories"))
+  screenshot(page, url: "http://127.0.0.1:3000/quotes", path: file_path("assets", "quotes"))
+  screenshot(page, url: "http://127.0.0.1:3000/themes", path: file_path("assets", "themes"))
 
   # Screenshot the homepage as each theme.
   Theme.standard.find_each do |theme|
-    Setting.find_by(slug: "theme").update!(value: theme.token)
-
-    screenshot(browser, url: "http://127.0.0.1:3000", path: "#{themes_dir}/#{theme.name.downcase}.png")
+    change_theme!(theme.token)
+    screenshot(page, url: "http://127.0.0.1:3000", path: file_path("assets/themes", theme.name.downcase))
   end
 end
