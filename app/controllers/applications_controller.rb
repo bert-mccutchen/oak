2ed6e6b5279abfd449368f0f7ba3c845e9e83b3d
Bert McCutchen <mail@bertm.dev>
2026-01-11 21:53:01 -0500

    Added application descriptions and missing line-clamps

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -48,6 +48,6 @@ class ApplicationsController < ApplicationController
   end
 
   def application_params
-    params.expect(application: %i[icon_id name url position])
+    params.expect(application: %i[icon_id name description url position])
   end
 end
