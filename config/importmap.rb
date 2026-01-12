4c1751ad8d164cf1baef89bf8bd63800172c14be
Bert McCutchen <mail@bertm.dev>
2026-01-11 20:29:50 -0500

    Updated JS dependencies

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,8 @@ pin "@hotwired/stimulus", to: "@hotwired--stimulus.js" # @3.2.2
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-sortable" # @4.1.1
-pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.12
+pin "@rails/request.js", to: "@rails--request.js.js" # @0.0.13
 pin "sortablejs" # @1.15.6
-pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@7.0.1/js/all.js" # @7.0.1
+pin "@fortawesome/fontawesome-free", to: "https://ga.jspm.io/npm:@fortawesome/fontawesome-free@7.1.0/js/all.js" # @7.1.0
 pin "strftime" # @0.10.3
 pin "@rolemodel/turbo-confirm", to: "@rolemodel--turbo-confirm.js" # @2.2.0
